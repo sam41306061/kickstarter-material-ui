7000c2a166d39ad0bf7657966a0c82aacc316c7b
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2022-12-03 11:10:09 -0700

    got name and value working in test

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -9,4 +9,13 @@ import "hardhat/console.sol";
 
 contract Campaign {
     string public name = "My Campagin 1";
+    uint256 public value = 18;
+    // address public recipient = '0x90e2b94532734E2999D4919Ea1b5263Bf35d4B0d';
+    // bool public complete = true;
+    // uint256 public approvalCount = 1;
+
+constructor(string memory _name, uint256 _value){
+    name = _name;
+    value = _value;
+}
 }
--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -9,10 +9,14 @@ describe("Campaign", () =>{
     beforeEach( async () =>{
         //Fetch the Contract
         const Campaign = await ethers.getContractFactory('Campaign');
-        camp = await Campaign.deploy();
-    })
+        camp = await Campaign.deploy('My Campagin 1', '18');
+    });
     it("Has a Name", async() =>{
         const name = await camp.name();
         expect(name).to.equal('My Campagin 1');
-    })
+    });
+    it("Has a value", async () =>{
+        const value = await camp.value();
+        expect( await camp.value()).to.equal('18');
+    });
 });
