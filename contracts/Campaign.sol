5d080235917d2acb7d1db86ca557383d219a7a54
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2022-12-28 21:19:59 -0700

    refactored working test case

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,35 +1,22 @@
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
-
 contract Campaign {
-    // string public description;
-    // address public recipient;
-    // bool public complete;
-    // uint approvalCount;
-    // uint value;
-     
-    //variables for contract
+    string public name;
+    // uint256 public minimum;
     address public manager;
-    uint public minimumContribution;
-    address[] public approvers; 
+    // address[] public approvers;
 
-    // Campgain setup
-    constructor(uint minimum) public {
-         manager = msg.sender;
-         minimumContribution = minimum;
-    }
-    // contribute
-    function contribute() public payable{
-        require(msg.value > minimumContribution);
+    
+    constructor(string memory _name, address _manager){
+        name = _name;
+        manager = _manager;
         
-        approvers.push(msg.sender);
     }
-    // createRequest
-    //approveRequest
-    //finalizeRequest
-  
+
+    // function contribute() public payable {
+    //     require(msg.value > minimum);
+
+    //     approvers.push(msg.sender);
+    // }
 }
-
-
-
--- test/Campaign.js
+++ test/Campaign.js
@@ -1,28 +1,29 @@
-const { defaultPath } = require("ethers/lib/utils");
+const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { add, result } = require("lodash");
+const { min } = require("moment/moment");
 
-describe ("Campaign", () => {
 
-    let manager = 'Campgain 1';
-    let minimumContribution = 18;
-   
-    // deployment test
-    describe('Deployment', () => {
-        it("this is the manager", async ()=> {
-            return manager; 
-        });
-        it('minimum amount set', async ()=> {
-            return minimumContribution;
-        });
-    });
 
-    // contribution
-    describe('Contribution', () => {
-        describe('Success', () =>{
-            const event = result.events[0];
-            expect(event.event).to.equal('contribute');
-        });
-    });
+describe("Campaign", () => {
+  let campaign;
+  let manager;
+ 
 
-}); 
+  
+  beforeEach(async() =>{
+    const Campaign  = await ethers.getContractFactory('Campaign');
+    campaign = await Campaign.deploy('Kickstart Me',"0xd115bffabbdd893a6f7cea402e7338643ced44a6");
+  });
+  it("get the contract name", async () => {
+    const name = await campaign.name();
+    expect(name).to.equal('Kickstart Me');
+  }); 
+  it("has a manager", async () => {
+    manager = await campaign.manager();
+    expect(manager).to.equal(manager);
+  });
+  // it("has a minimum contribution", async () => {
+  //   minimum = await campaign.minimumContribution();
+  //   expect(minimum).to.equal(minimum);
+  // });
+});
