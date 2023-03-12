c77d87261b7a71812365b948f71f187ad07a9353
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-03-12 09:28:24 -0600

    working test configuration

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,22 +1,52 @@
 pragma solidity ^0.8.0;
+
 import "hardhat/console.sol";
 
 contract Campaign {
-    string public name;
-    // uint256 public minimum;
     address public manager;
-    // address[] public approvers;
+    uint public minimumContribution;
+    address[] public approvers;
+    uint public numRequests;
+    mapping(uint => Request) public requests;
 
-    
-    constructor(string memory _name, address _manager){
-        name = _name;
-        manager = _manager;
-        
+
+    struct Request {
+        string description;
+        uint value;
+        address payable recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
-    // function contribute() public payable {
-    //     require(msg.value > minimum);
+    constructor(uint _minimumContribution, address _manager) {
+        minimumContribution = _minimumContribution;
+        manager = _manager;
+    }
 
-    //     approvers.push(msg.sender);
-    // }
+    function contribute() public payable {
+        require(msg.value >= minimumContribution, "Contribution amount is less than minimum");
+        approvers.push(msg.sender);
+    }
+
+    function createRequest(string memory _description, uint _value, address payable _recipient) public onlyManager {
+        require(_value <= address(this).balance, "Not enough funds available");
+        Request storage newRequest = requests[numRequests];
+        newRequest.description = _description;
+        newRequest.value = _value;
+        newRequest.recipient = _recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+        numRequests++;
+    }
+
+    // approveRequest
+
+    // finalizeRequest
+
+    // can be called before or after a function call. sets managers as owner. 
+     modifier onlyManager() {
+        require(msg.sender == manager, "Only the manager can call this function");
+        _;
+    }
 }
--- test/Campaign.js
+++ test/Campaign.js
@@ -1,29 +1,87 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
-const { min } = require("moment/moment");
 
-
-
-describe("Campaign", () => {
+describe("Campaign", function() {
+  let Campaign;
   let campaign;
-  let manager;
- 
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
-  
-  beforeEach(async() =>{
-    const Campaign  = await ethers.getContractFactory('Campaign');
-    campaign = await Campaign.deploy('Kickstart Me',"0xd115bffabbdd893a6f7cea402e7338643ced44a6");
+  beforeEach(async function() {
+    Campaign = await ethers.getContractFactory("Campaign");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    campaign = await Campaign.deploy(100, owner.address);
+    await campaign.deployed();
   });
-  it("get the contract name", async () => {
-    const name = await campaign.name();
-    expect(name).to.equal('Kickstart Me');
-  }); 
-  it("has a manager", async () => {
-    manager = await campaign.manager();
-    expect(manager).to.equal(manager);
+
+  describe("Deployment", function() {
+    it("Should set the right owner and minimumContribution", async function() {
+      expect(await campaign.manager()).to.equal(owner.address);
+      expect(await campaign.minimumContribution()).to.equal(100);
+    });
   });
-  // it("has a minimum contribution", async () => {
-  //   minimum = await campaign.minimumContribution();
-  //   expect(minimum).to.equal(minimum);
+
+  describe("Contribute", function() {
+    it("Should add the sender to the approvers list", async function() {
+      await campaign.contribute({ value: 200 });
+      expect(await campaign.approvers(0)).to.equal(owner.address);
+    });
+
+    it("Should throw an error if contribution amount is less than minimum", async function() {
+      await expect(campaign.contribute({ value: 50 })).to.be.revertedWith("Contribution amount is less than minimum");
+    });
+  });
+
+  describe("Create Request", function() {
+    it("Should create a request", async function() {
+      await campaign.contribute({ value: 200 });
+      await campaign.createRequest("Computer system update", 150, addr1.address);
+
+      const request = await campaign.requests(0);
+      expect(request.description).to.equal("Computer system update");
+      expect(request.value).to.equal(150);
+      expect(request.recipient).to.equal(addr1.address);
+      expect(request.complete).to.equal(false);
+      expect(request.approvalCount).to.equal(0);
+    });
+
+    it("Should throw an error if value is greater than the contract balance", async function() {
+      await expect(campaign.createRequest("Computer system update", 500, addr1.address)).to.be.revertedWith("Not enough funds available");
+    });
+  });
+
+  // describe("Approve Request", function() {
+  //   it("Should approve a request", async function() {
+  //     await campaign.contribute({ value: 200 });
+  //     await campaign.createRequest("Computer system update", 150, addr1.address);
+
+  //     await campaign.approveRequest(0);
+
+  //     const request = await campaign.requests(0);
+  //     expect(request.approvals[owner.address]).to.equal(true);
+  //     expect(request.approvalCount).to.equal(1);
+  //   });
   // });
+
+  // describe("Finalize Request", function() {
+  //   it("Should finalize a request", async function() {
+  //     await campaign.contribute({ value: 200 });
+  //     await campaign.createRequest("Buy a new laptop", 150, addr1.address);
+  //     await campaign.approveRequest(0);
+  //     await campaign.finalizeRequest(0);
+
+  //     const request = await campaign.requests(0);
+  //     expect(request.complete).to.equal(true);
+  //   });
+
+  //   it("Should throw an error if request has not been approved by more than half of the approvers", async function() {
+  //     await campaign.contribute({ value: 200 });
+  //     await campaign.createRequest("Buy a new laptop", 150, addr1.address);
+
+  //     await expect(campaign.finalizeRequest(0)).to.be.revertedWith("Not enough approvals");
+  //   });
+  // });
+
 });
