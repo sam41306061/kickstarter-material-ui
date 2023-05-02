336e6746942d9b06cda905fb7a9700eb30da25ca
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-05-01 18:32:37 -0600

    udpated the deploy script to function correclty

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,28 @@
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying contracts with the account:", deployer.address);
+  const signers = await ethers.getSigners();
+  console.log("Using signers:", signers.map(signer => signer.address));
 
   const campaignNames = ["Campaign 1", "Campaign 2", "Campaign 3"];
-    const campaignFundingGoals = [100, 200, 300]; // in ether
+  const campaignFundingGoals = [100, 200, 300]; // in ether
+  const campaignAddresses = [
+    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+    "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
+  ];
 
-    for (let i = 0; i < campaignNames.length; i++) {
-      const Campaign = await ethers.getContractFactory("Campaign");
-      const campaign = await Campaign.deploy(campaignFundingGoals[i], deployer.address);
-      console.log(`Deploying ${campaignNames[i]}...`);
-      await campaign.deployed();
-      console.log(`${campaignNames[i]} deployed at: ${campaign.address}`);
+  for (let i = 0; i < campaignNames.length; i++) {
+    // Get the signer for the specified address
+    const signer = signers.find(s => s.address.toLowerCase() === campaignAddresses[i].toLowerCase());
+    if (!signer) {
+      console.error(`No signer found for address ${campaignAddresses[i]}. Skipping deployment of ${campaignNames[i]}.`);
+      continue;
+    }
+
+    const Campaign = await ethers.getContractFactory("Campaign", signer);
+    const campaign = await Campaign.deploy(campaignFundingGoals[i], signer.address);
+    console.log(`Deploying ${campaignNames[i]}...`);
+    await campaign.deployed();
+    console.log(`${campaignNames[i]} deployed at: ${campaign.address}`);
   }
 
   // const Campaign = await ethers.getContractFactory("Campaign");
